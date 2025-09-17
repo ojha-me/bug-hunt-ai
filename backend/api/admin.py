000d66d0fe12e3ecba1f4a6bf78b1f484ce832d1
Dinesh Ojha <dineshojha1013@gmail.com>
2025-09-17 09:04:52 +0545

    nit.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Challenge, CodeSnippet, Session, Hint, Message, Submission, UserProgress
+admin.site.register(User)
+admin.site.register(Challenge)
+admin.site.register(CodeSnippet)
+admin.site.register(Session)
+admin.site.register(Hint)
+admin.site.register(Message)
+admin.site.register(Submission)
+admin.site.register(UserProgress)
+
--- backend/api/models.py
+++ backend/api/models.py
@@ -15,14 +15,14 @@ class User(AbstractUser):
     # Override related_name to avoid clashes
     groups = models.ManyToManyField(
         'auth.Group',
-        related_name='custom_user_set',  # <--- change this
+        related_name='custom_user_set',  
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
-        related_name='custom_user_permissions_set',  # <--- change this
+        related_name='custom_user_permissions_set', 
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
